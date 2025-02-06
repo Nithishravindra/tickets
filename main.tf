78f11601118f7dafd125f0a39629eea18ca69b3e
nithish-ravindra <nithish.ravindra@hashicorp.com>
2025-02-06 17:03:40 +0530

    troubleshooting tf - 2

--- main.tf
+++ main.tf
@@ -1,14 +1,23 @@
 terraform { 
-  cloud { 
+    cloud { 
     
-    organization = "tce-icebreaker" 
+        organization = "tce-icebreaker" 
 
-    workspaces { 
-      name = "policy" 
+        workspaces { 
+        name = "policy" 
+        } 
     } 
-  } 
+    required_providers {
+        random = {
+            source  = "hashicorp/random"
+            version = "3.0.1"
+        }
+        aws = {
+            source  = "hashicorp/aws"
+            version = ">= 3.26.0"
+        }
+    }
 }
-
 provider "aws" {
   region = "us-east-1"
 }
--- resources.tf
+++ resources.tf
@@ -15,3 +15,27 @@ resource "aws_iam_role" "lambda_role" {
     ]
   })
 }
+
+resource "aws_instance" "ubuntu" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default = "Provisioned by Terraform"
+}
+
+output "instance_ami" {
+  value = aws_instance.ubuntu.ami
+}
+
+output "instance_arn" {
+  value = aws_instance.ubuntu.arn
+}
