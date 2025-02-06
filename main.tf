991d59d82826b76f3052239e672d71cc48bb0791
nithish-ravindra <nithish.ravindra@hashicorp.com>
2025-02-06 17:04:39 +0530

    troubleshooting tf - 3

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
 terraform { 
     cloud { 
-    
         organization = "tce-icebreaker" 
-
         workspaces { 
         name = "policy" 
         } 
     } 
-    required_providers {
-        random = {
-            source  = "hashicorp/random"
-            version = "3.0.1"
-        }
-        aws = {
-            source  = "hashicorp/aws"
-            version = ">= 3.26.0"
-        }
-    }
 }
 provider "aws" {
   region = "us-east-1"
