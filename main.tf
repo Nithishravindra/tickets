d54e4321830f1b303bf347fa7127114ed6bb47bb
Nithish R <nithish.ravindra@hashicorp.com>
2025-02-24 12:17:47 +0530

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform { 
-    cloud { 
-        organization = "tce-icebreaker" 
-        workspaces { 
-        name = "policy" 
-        } 
-    } 
-}
 provider "aws" {
   region = "us-east-1"
 }
